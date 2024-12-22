52d935cfaaa377f35c8b15de40a06597b6a97c3e
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-12-22 09:30:12 -0500

    fix linting thing

--- src/app/demoPage/actions.ts
+++ src/app/demoPage/actions.ts
@@ -63,6 +63,7 @@ export async function getDemoPageContent(
   }
 
   // Remove password before returning the content
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { password: _, ...pageContent } = page;
   return pageContent;
 }
